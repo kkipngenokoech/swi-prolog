cf00b9858c39a0f37a6fa761b0622776a66715a0
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-02-09 20:59:07 +0300

    added a whitespace

--- school.pl
+++ school.pl
@@ -11,6 +11,7 @@ goal(boston, improve_performance(maths)).
 goal(sitienei, help(boston)).
 goal(chesebe, monitor(results)).
 #actions
+
 execute(boston, attend_tutor_lessons):-goal(boston, improve_performance(maths)).
 execute(sitienei, create_tutor_lessons):-goal(sitienei, help(boston)), believes(chesebe, aware_of_performance).
 execute(chesebe, request_students_performance):-goal(chesebe, monitor(results)).
